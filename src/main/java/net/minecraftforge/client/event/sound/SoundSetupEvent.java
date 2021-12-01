e46512af2e8876577c98c9413a023fbd036d874e
Jonathan Coates <git@squiddev.cc>
2021-12-01 23:23:36 +0000

    Start firing SoundSetupEvent instead
    In 1.17 the constructor of SoundEngine was (assumedly inadvertently)
    changed to fire a SoundLoadEvent instead of a SoundSetupEvent.

--- src/main/java/net/minecraftforge/client/event/sound/SoundSetupEvent.java
+++ src/main/java/net/minecraftforge/client/event/sound/SoundSetupEvent.java
@@ -20,12 +20,13 @@
 package net.minecraftforge.client.event.sound;
 
 import net.minecraft.client.sounds.SoundEngine;
+import net.minecraftforge.fml.event.IModBusEvent;
 
 /**
  * This event is raised by the SoundManager when it does its first setup of the
  * SoundSystemConfig's codecs, use this function to add your own codecs.
  */
-public class SoundSetupEvent extends SoundEvent
+public class SoundSetupEvent extends SoundEvent implements IModBusEvent
 {
     public SoundSetupEvent(SoundEngine manager)
     {
